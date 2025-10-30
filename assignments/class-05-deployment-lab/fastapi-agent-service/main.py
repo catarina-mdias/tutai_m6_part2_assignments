8d39e675d3e1e112287b1f05c8987fa36c851509
Catarina Dias <catarina.m.dias8@gmail.com>
2025-10-30 22:23:12 +0000

    assignment class 5 - correct session id

--- assignments/class-05-deployment-lab/fastapi-agent-service/main.py
+++ assignments/class-05-deployment-lab/fastapi-agent-service/main.py
@@ -181,7 +181,7 @@ def invoke_agent(message: str, langfuse_client: Langfuse, session_id: str) -> tu
     with langfuse_client.start_as_current_span(
             name="🤖-fastapi-agent"
     ) as span:
-        span.update_trace(input=message, session_id="chat_tutai_123")
+        span.update_trace(input=message, session_id=session_id)
 
         agent_reply = run_agent(message)
 
